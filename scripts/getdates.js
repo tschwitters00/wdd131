22d71872a994505c585b115589645e53177473c9
tschwitters00 <trev16@rocketmail.com>
2026-07-03 15:15:12 -0500

    W01 Assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const year = today.getFullYear();
+
+document.getElementById("currentyear").innerHTML = year;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
