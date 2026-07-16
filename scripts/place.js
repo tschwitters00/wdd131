30831b20d71fb9bfbdedd1152599a0696736cf71
tschwitters00 <trev16@rocketmail.com>
2026-07-16 14:21:04 -0500

    re redeploy

--- scripts/place.js
+++ scripts/place.js
@@ -3,7 +3,7 @@ const conditions = "Cloudy"
 const wind = 2
 
 function calculateWindChill(temp, wind) {
-    return Math.round(35.74 + 0.6215*temp - 35.75*wind**0.16 + 0.4275*temp*wind**0.16);
+    return Math.round(35.74 + 0.6215*temp - 35.75*wind**0.16 + 0.4275*temp*wind**0.16); 
 }
 
 if (temperature <= 50 && wind > 3) {
