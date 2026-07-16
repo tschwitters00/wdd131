193e76699692d022e17364b22700afe0347c3146
tschwitters00 <trev16@rocketmail.com>
2026-07-16 02:17:28 -0500

    more fixes

--- scripts/place.js
+++ scripts/place.js
@@ -3,20 +3,19 @@ const conditions = "Cloudy"
 const wind = 2
 
 function calculateWindChill(temp, wind) {
-    if (temp <= 50 && wind > 3) {
-        var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16))
-        windChill = Math.round(windChill);
-        let chill = windChill.toString();
-        return chill + "°F"
-    }
-
-    else {
-        return "N/A"
-    }
-
+    var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wind,0.16)) + (0.4275*temp*Math.pow(wind,0.16))
+    windChill = Math.round(windChill);
+    let chill = windChill.toString();
+    return chill + "°F"
 }
 
-var windChill = calculateWindChill(temperature, wind)
+if (temp <= 50 && wind > 3) {
+    var windChill = calculateWindChill(temperature, wind)
+}
+
+else {
+    let windChill = "N/A"
+}
 
 document.getElementById("temperature").innerHTML = temperature;
 document.getElementById("conditions").innerHTML = conditions;
